97458c51abd469d837219cf1f5556a29eb17891a
BikashGhale773 <ghalebikash445@gmail.com>
2023-11-29 15:20:10 -0600

    checking

--- src/main/java/com/example/RestAPI/Calculator.java
+++ src/main/java/com/example/RestAPI/Calculator.java
@@ -1,12 +1,14 @@
 package com.example.RestAPI;
 
 public class Calculator {
-    public int sum(int a, int d){
-        return a + d;
-    }
+//    public int sum(int a, int d){
+//        return a + d;
+//    }
 
     public static void main(String[] args){
-        Calculator calc = new Calculator();
-        calc.sum(2,2);
+//        Calculator calc = new Calculator();
+//        calc.sum(2,2);
+
+        System.out.println("Hello World");
     }
 }
